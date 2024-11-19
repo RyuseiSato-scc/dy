8e2d18e635fd3e84e17cfce7c3c36217dfebb606
佐藤瑠星 <ryusei@satounoMacBook-Pro-2.local>
2024-11-19 10:11:55 +0900

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "dypjfom": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
